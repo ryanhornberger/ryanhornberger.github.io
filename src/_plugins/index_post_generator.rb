424fa815b888be159b9f43511ebe66508cde2697
Ryan Hornberger <ryan@ryanhornberger.com>
2014-08-29 19:00:10 -0600

    Gives the title page its own title and description

--- src/_plugins/index_post_generator.rb
+++ src/_plugins/index_post_generator.rb
@@ -1,22 +1,5 @@
 module Jekyll
 
-  class IndexPostPage < Page
-    def initialize(site, base, last_post)
-      @site = site
-      @base = base
-      @dir  = "/"
-
-      last_name = last_post.name
-      dot_index = last_name.rindex('.')
-      @name = "index#{last_name[dot_index..-1]}"
-
-      process(name)
-
-      self.data = last_post.data
-      self.content = last_post.content
-    end
-  end
-
   class IndexPostPageGenerator < Generator
     safe true
 
@@ -29,26 +12,18 @@ module Jekyll
             if page.data.has_key?('generate_from_post') && page.data['generate_from_post']              
               last_post = site_posts.last
 
-              page.data = last_post.data
+              title = page.data['title'] || 'Index Page'
+              description = page.data['description'] || 'Index Description'
+
+              page.data = last_post.data.dup
+              page.data['title'] = title
+              page.data['description'] = description
+
               page.content = last_post.content
             end
           end
         end
       end
-      
-
-      #site.pages.each do |page|
-      #  if page.url.start_with? '/index.'
-      #    return
-      #  end
-      #end
-      #site_posts = site.posts
-      #
-      #if site_posts.count == 0
-      #  return
-      #end
-      #
-      #site.pages << IndexPostPage.new(site, site.source, site_posts.last)
     end
   end
 
--- src/index.md
+++ src/index.md
@@ -1,5 +1,7 @@
 ---
 generate_from_post: true
+title: Ryan Hornberger - Web Product Entrepreneur 
+description: I'm currently the VP of Scan to Pay at Scan Inc. I design and build customer facing web and mobile apps.
 ---
 
 This is the default index page
