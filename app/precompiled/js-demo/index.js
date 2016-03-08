5812339b430422dce7075193b71716496b57ca22
Ryan Hornberger <ryan@ryanhornberger.com>
2016-03-07 17:40:49 -0800

    incomplete

--- app/precompiled/js-demo/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import RenderBoxes from 'jsdemo/components/RenderBoxes.js';
-
-var boxes = new RenderBoxes(200, 200).render();
-var drawSpace = document.getElementById('js-demo-space').innerHTML = boxes;
