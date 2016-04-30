0f32afd97324a43418c0cd3aa16eb23556ed2ccb
Ryan Hornberger <ryan@ryanhornberger.com>
2016-04-30 16:32:18 -0700

    nearly complete on the deploy process

---
+++ bin/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo '--------------'
+echo 'deploying to github'
+echo '--------------'
+
+echo ''
+echo 'building'
+echo '--------------'
+
+node --harmony $DIR/../node_modules/webpack/bin/webpack.js --config $DIR/../webpack.config.babel.js 
+
+echo ''
+echo 'deploying'
+echo '--------------'
+
+rm -rf $DIR/../../tmp/
+mkdir $DIR/../../tmp/
+mv $DIR/../_compiled/public/* $DIR/../../tmp/
+git checkout master
+rm -rf ./*
+rm -rf ./.sass-cache
+mv $DIR/../../tmp/* ./
+
+#
+#      system "mv dist/* #{tmp}"
+#      system 'git checkout master'
+#      system 'rm -rf ./*'
+#      system 'rm -rf ./.sass-cache'
+#      system "mv #{tmp}/* ."
+#      message = "Site updated at #{Time.now.utc}"
+#      system 'git add .'
+#      system "git commit -am #{message.shellescape}"
+#      system 'git push origin master'
+#      system 'git checkout development'
+#      system "echo 'Deployed to GitHub'"#
