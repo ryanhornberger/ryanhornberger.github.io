9f60d52d13ca63686c40d971e317bbbcbe53333c
Ryan Hornberger <ryan@ryanhornberger.com>
2016-08-20 19:37:37 -0700

    improvements to callable hours

--- source/com/ryanhornberger/routes/portfolio/1/PortfolioOnePage.jsx
+++ source/com/ryanhornberger/routes/portfolio/1/PortfolioOnePage.jsx
@@ -6,9 +6,13 @@ import Helmet from 'react-helmet';
 
 // Configurations
 var canvasHW = 200;
+
 var clockCircleRadius = 93;
 var clockCircleDayColor = "#eeeeee";
 var clockCircleNightColor = "#aaaaaa";
+
+var callableHoursRadius = 87;
+var callableHoursColor = "#eeeeee";
 
 // Calculations
 var halfwayHW = canvasHW / 2;
@@ -20,17 +24,26 @@ var viewBox = function(){
 var clockCircleArcStartPosition = function(){
 	return `${halfwayHW - clockCircleRadius} ${halfwayHW}`;
 }();
-
 var clockCircleArcEndPosition = function(){
 	return `${halfwayHW + clockCircleRadius} ${halfwayHW}`;
 }();
-
 var clockCircleCircumference = Math.PI*(clockCircleRadius*2);
-
 var clockCircleStrokeDasharray = function(){
 	var strokeSize = clockCircleCircumference / 24; // (24 hours per day)
 	return "" + (strokeSize - 1) + ",1"; //We are going to give 1 to the space in between
 }();
+
+var callableHoursCircumference = Math.PI*(callableHoursRadius*2);
+var callableHoursStrokeDashoffset = function(){
+	return callableHoursCircumference / 3 * 2;
+}();
+var callableHoursStrokeDasharray = function() {
+	return `0,${(callableHoursCircumference/24*15)},${(callableHoursCircumference/24*8)-1},${(callableHoursCircumference/24*1)+1}`;
+}();
+var nonCallableHoursStrokeDasharray = function() {
+	return `0,${callableHoursCircumference/24*4},${(callableHoursCircumference/24*10)-1},${(callableHoursCircumference/24*10)+1}`;
+}();
+
 
 class PortfolioOnePage extends React.Component 
 {
@@ -64,19 +77,22 @@ class PortfolioOnePage extends React.Component
 						<svg width="100%" viewBox={viewBox}>
 							
 							{/*Clock top - day hours - lighter*/}
-							<path fill="transparent" stroke={clockCircleDayColor} strokeWidth="6" strokeDasharray={clockCircleStrokeDasharray} strokeDashoffset="0" d={`
+							<path stroke={clockCircleDayColor} strokeWidth="6" strokeDasharray={clockCircleStrokeDasharray} strokeDashoffset="0" fill="transparent" d={`
 								M ${clockCircleArcStartPosition}
 								A ${clockCircleRadius} ${clockCircleRadius} 0 0 1 ${clockCircleArcEndPosition}
 							`} />
 
 							{/*Clock bottom - night hours - darker*/}
-							<path fill="transparent" stroke={clockCircleNightColor} strokeWidth="6" strokeDasharray={clockCircleStrokeDasharray} strokeDashoffset="-1" d={`
+							<path stroke={clockCircleNightColor} strokeWidth="6" strokeDasharray={clockCircleStrokeDasharray} strokeDashoffset="-1" fill="transparent" d={`
 								M ${clockCircleArcStartPosition}
 								A ${clockCircleRadius} ${clockCircleRadius} 0 0 0 ${clockCircleArcEndPosition}
 							`} />
 
-							{/*Next step*/}
-							<circle cx={halfwayHW} cy={halfwayHW} r="99" fill="transparent" stroke="#eeeeee" strokeWidth="2" strokeDashoffset="300" strokeDashoffset="300" />
+							{/*Callable hours*/}
+							<circle cx={halfwayHW} cy={halfwayHW} r={callableHoursRadius} stroke={clockCircleDayColor} strokeWidth="2" strokeDasharray={callableHoursStrokeDasharray} fill="transparent" />
+							
+							{/*Non-callable hours*/}
+							<circle cx={halfwayHW} cy={halfwayHW} r={callableHoursRadius} stroke={clockCircleNightColor} strokeWidth="2" strokeDasharray={nonCallableHoursStrokeDasharray} fill="transparent" />
 							
 						</svg>
 					</div>
